dab9b18d0a7e73a4585f9204ca4b02cccd12e765
Alfred Myrne <alfredmyrneblader@gmail.com>
2020-05-22 15:10:29 +0200

    Fkar Runt Med Javascript

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,11 @@
 var modal = document.getElementById("myModal");
 var slides = document.getElementsByClassName("mySlides");
+var vids = document.getElementById("video1")
 
 function openModal() {
   modal.style.display = "block";
 }
+
 
 window.onclick = function(event) {
   if (event.target == modal) {
@@ -28,9 +30,31 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+
+// function play(n) {
+//   var vid = slides[n].querySelector('video');
+//   vid.play();
+// }
+//
+// function pause(n) {
+//   var vid = slides[n].querySelector('video');
+//   vid.pause();
+// }
+//
+// function isVideo(n) {
+//
+//   var type = slides[n].childNodes[0].tagName.toLowerCase();
+//   if(type === 'video'){
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+
 function showSlides(n) {
   var i;
-  
+
   var dots = document.getElementsByClassName("demo");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
@@ -40,6 +64,14 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
+
+
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+
+  var elem = slides[slideIndex-1].querySelector('video'); //first child of current slide
+  if (elem) {
+    elem.play();
+  }
+
 }
